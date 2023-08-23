d5bc43d257743a3afa4dcb0521928b4ccd5596c4
Nadine Fahmy <125144049+NadineFahmy@users.noreply.github.com>
2023-08-23 15:31:06 -0500

    card_code_access.ino

--- card_code_access.ino
+++ card_code_access.ino
@@ -1,12 +1,10 @@
-#include <LiquidCrystal_I2C.h>
-#include <Wire.h>
+
 #include <SPI.h>
 #include <MFRC522.h>
 #define SS_PIN 10
 #define RST_PIN 9
 #define Buzzer 5
 #define lock 7
-LiquidCrystal_I2C Lcd(0x27, 16, 2);
 
 MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
 bool flag=false;
@@ -14,8 +12,6 @@ bool flag1=false;
  
 void setup() 
 {
-  Lcd.init();
-  Lcd.backlight();
   Serial.begin(9600);
   SPI.begin();      // Initiate  SPI bus
   rfid.PCD_Init(); // Init MFRC522 
@@ -64,21 +60,17 @@ void rfid1()
   UID.toUpperCase();
   // E8 07 C7 D2
   if (UID.substring(1) == "EA 5D 1F 3F" || UID.substring(1) == "C4 33 9A 28") //change here the UID of the card/cards that you want to give access
-  {
-    
-    Serial.println("KIMO");
+  
     Serial.println("Authorized access");
-    Lcd.print("Authorized access");
     flag=true;
   }else if(UID.substring(1) == "AA E4 25 40")
   {
-    Serial.println("karim");
+   
     Serial.println("Authorized access");
     flag=true;
   }else
   {
     Serial.println("Access denied");
-    Lcd.print("Access denied");
     digitalWrite(Buzzer,HIGH);
     delay(500);
     digitalWrite(Buzzer,LOW);
